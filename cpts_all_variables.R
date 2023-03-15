fa3c02f4e81b4110e1bfb04f13a0284ea7544542
Whalen <mwhalen3@luc.edu>
2023-03-15 12:44:35 -0500

    update eras results

--- cpts_all_variables.R
+++ cpts_all_variables.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(janitor)
 library(hdbinseg)
 
+
 teams <- read_csv("data/Teams.csv") %>% 
   clean_names()
 
@@ -13,30 +14,47 @@ team_names <- teams %>%
 
 old_teams_full <- teams %>% 
   filter(franch_id %in% team_names & year_id > 1900) %>% 
-  select(year_id, franch_id, w, l, r, ab, h, hr, bb, so)
+  select(year_id, franch_id,g, r, ab, sb, hr, bb, so, attendance)
 
 names(old_teams_full)[-c(1:2)]
+## 16 missing values from SO
+apply(old_teams_full, 2,function(x) sum(is.na(x)))
 
-cpt <- function(dataset, var){
-  wide <- dataset %>% 
-    pivot_wider(id_cols = franch_id, 
-                names_from = year_id, 
-                values_from = var)
-  
-  ## find thresholding value for cpt using bootstrapping
-  thrs <- dcbs.thr(as.matrix(wide[-1]), interval = c(1, dim(wide[-1])[2]))
-  ### if I lower the threshold more changepoints
-  hr_cpt_dcbs <- dcbs.alg(as.matrix(wide[-1]), cp.type = 1, thr = thrs)
-  cpt_years <- as.numeric(names(wide[-1])[hr_cpt_dcbs$ecp])
-  return(list(threshold = thrs,
-              years = cpt_years),
-              variable = var)
-  
-}
 
-changes <- lapply(names(old_teams_full)[-c(1:2,10)], 
+imp <- mice::mice(old_teams_full[-c(1:2)])
+teams_full_nomiss <- complete(imp)
+apply(teams_full_nomiss, 2,function(x) sum(is.na(x)))
+
+full_teams <- bind_cols(old_teams_full[1:2],teams_full_nomiss) %>% 
+  mutate(hr_per_game = hr/g, 
+         so_per_game = so/g,
+         bb_per_game = bb/g,
+         att_per_game = attendance/g,
+         sb_per_game = sb/g, 
+         r_per_game = r/g) %>% 
+  select(year_id, franch_id, hr_per_game, so_per_game, bb_per_game, att_per_game, sb_per_game, r_per_game)
+
+
+# cpt <- function(dataset, var){
+#   wide <- dataset %>% 
+#     pivot_wider(id_cols = franch_id, 
+#                 names_from = year_id, 
+#                 values_from = var)
+#   
+#   ## find thresholding value for cpt using bootstrapping
+#   thrs <- dcbs.thr(as.matrix(wide[-1]), interval = c(1, dim(wide[-1])[2]))
+#   ### if I lower the threshold more changepoints
+#   hr_cpt_dcbs <- dcbs.alg(as.matrix(wide[-1]), cp.type = 1, thr = thrs)
+#   cpt_years <- as.numeric(names(wide[-1])[hr_cpt_dcbs$ecp])
+#   return(list(threshold = thrs,
+#               years = cpt_years),
+#               variable = var)
+#   
+# }
+
+changes <- lapply(names(full_teams)[-c(1:2)], 
                   function(i){
-                    wide <- old_teams_full %>% 
+                    wide <- full_teams %>% 
                       pivot_wider(id_cols = franch_id, 
                                   names_from = year_id, 
                                   values_from = i)
@@ -44,8 +62,8 @@ changes <- lapply(names(old_teams_full)[-c(1:2,10)],
                     ## find thresholding value for cpt using bootstrapping
                     thrs <- dcbs.thr(as.matrix(wide[-1]), interval = c(1, dim(wide[-1])[2]))
                     ### if I lower the threshold more changepoints
-                    hr_cpt_dcbs <- dcbs.alg(as.matrix(wide[-1]), cp.type = 1, thr = thrs -3)
-                    cpt_years <- as.numeric(names(wide[-1])[hr_cpt_dcbs$ecp])
+                    cpt_dcbs <- dcbs.alg(as.matrix(wide[-1]), cp.type = 1, thr = thrs-1)
+                    cpt_years <- as.numeric(names(wide[-1])[cpt_dcbs$ecp])
                     return(tibble(threshold = thrs,
                                 years = cpt_years,
                                 variable = i))
@@ -57,7 +75,7 @@ time_cpts <- changes %>%
   unnest(cols = c(.)) 
 
 
-old_teams_full %>% 
+full_teams %>% 
   pivot_longer(cols = -c(year_id, franch_id), names_to = "variable", values_to = "value") %>% 
   ggplot(aes(year_id, value, color = franch_id)) +
   geom_line() +
--- hr_cpt.R
+++ hr_cpt.R
@@ -19,7 +19,7 @@ teams %>%
 
 old_teams_full <- teams %>% 
   filter(franch_id %in% team_names & year_id > 1900) %>% 
-  select(year_id, franch_id,hbp, e, sb, w, l, r, ab, h, hr, bb, so,g, attendance,era) %>% 
+  select(year_id, franch_id,hbp, e, sb, l, r, ab, h, hr, bb, so,g, attendance,era) %>% 
   mutate(hr_per_game = hr/g, 
          so_per_game = so/g,
          bb_per_game = bb/g,
@@ -35,12 +35,12 @@ old_teams_full <- teams %>%
 teams_wide <- old_teams_full %>% 
   pivot_wider(id_cols = franch_id, 
               names_from = year_id, 
-              values_from = hr_per_game)
+              values_from = att_per_game)
 
 ## find thresholding value for cpt using bootstrapping
 thrs <- dcbs.thr(as.matrix(teams_wide[-1]), interval = c(1, dim(teams_wide[-1])[2]))
 ### if I lower the threshold more changepoints
-hr_cpt_dcbs <- dcbs.alg(as.matrix(teams_wide[-1]), cp.type = 1, thr = thrs )
+hr_cpt_dcbs <- dcbs.alg(as.matrix(teams_wide[-1]), cp.type = 1, thr = thrs)
 hr_cpt_dcbs$ecp
 hr_cpt_sbs <- sbs.alg(as.matrix(teams_wide[-1]), cp.type = 1)
 hr_cpt_sbs$ecp
@@ -48,7 +48,9 @@ hr_cpt_sbs$ecp
 cpt_years <- as.numeric(names(teams_wide[-1])[hr_cpt_dcbs$ecp])
 cpt_years
 
-ggplot(old_teams_full, aes(year_id, hr_per_game, color = franch_id)) +
+ggplot(old_teams_full, aes(year_id, att_per_game, color = franch_id)) +
   geom_line() +
   geom_vline(xintercept = cpt_years)
+
+
 
