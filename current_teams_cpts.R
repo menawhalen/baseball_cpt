0b9ba65b4da2797b2161832ea643814c74c59db1
Whalen <mwhalen3@luc.edu>
2023-03-28 17:43:05 -0500

    update current teams for proper variables to use and standardized properly. Pdf of each time series given.

--- current_teams_cpts.R
+++ current_teams_cpts.R
@@ -12,81 +12,41 @@ current_teams <- teams %>% filter(year_id == 2021) %>% pull(franch_id)
 teams %>% 
   filter(franch_id %in% current_teams) %>% 
   map(~sum(is.na(.)))
-
-imp <- mice::mice(filter(teams, franch_id %in% current_teams)[15:40])
+## missing values in so and sb that we need to fill in
+imp <- mice::mice(teams[c("r","h","hr","bb","so", "ab", "sb", 
+                          "ra", "ha", "hra", "bba", "soa")], method = "cart")
 teams_full_nomiss <- complete(imp)
-apply(teams_full_nomiss, 2,function(x) sum(is.na(x)))
+#apply(teams_full_nomiss, 2,function(x) sum(is.na(x)))
 
-clean_modern_teams <- bind_cols(filter(teams, franch_id %in% current_teams)[1:14],
-                                teams_full_nomiss,
-          filter(teams, franch_id %in% current_teams)[41:48])
+teams_full_nomiss$bb[imp$where[,c("bb")]] #no negative values now capped at "real" values
+teams_full_nomiss$sb[imp$where[,c("sb")]]
+teams_full_nomiss$so[imp$where[,c("so")]]
 
-## divide by season average for team stats ??
-clean_modern_teams %>% 
-  group_by(year_id) %>% 
-  mutate(r_season = mean(r),
-         ab_season = mean(ab),
-         h_season = mean(h),
-         hr_season = mean(hr),
-         bb_season = mean(bb),
-         so_season = mean(so),
-         sb_season = mean(sb),
-         w_season = mean(w),
-         ha_season = mean(ha),
-         bba_season = mean(bba),
-         soa_season = mean(soa),
-         hra_season = mean(hra),
-         att_season = mean(attendance),
-         r_per_season = r/r_season,
-         ab_per_season = ab/ab_season,
-         h_per_season = h/h_season,
-         hr_per_season = hr/hr_season,
-         bb_per_season = bb/bb_season,
-         so_per_season = so/so_season,
-         sb_per_season = sb/sb_season,
-         hra_per_season = hra/hra_season,
-         w_per_season = w/w_season,
-         ha_per_season = ha/ha_season,
-         bba_per_season = bba/bba_season,
-         soa_per_season = soa/soa_season,
-         att_per_season = attendance/att_season) %>% 
-  pivot_longer(cols = ends_with("per_season"), names_to = "stat_type", values_to = "stat") %>% 
-  ggplot(aes(year_id, stat, color = stat_type)) +
-  geom_line() +
-  facet_wrap(~franch_id, scales = "free_x")
+clean_teams <- bind_cols(teams[c("year_id", "franch_id")],
+                                teams_full_nomiss)
 
 ### standardize teams by season average then by number of games
-std_teams <- clean_modern_teams %>% 
+std_teams <- clean_teams %>% 
   group_by(year_id) %>% 
-  mutate(r_season = mean(r),
-         ab_season = mean(ab),
-         h_season = mean(h),
-         hr_season = mean(hr),
-         bb_season = mean(bb),
-         so_season = mean(so),
-         sb_season = mean(sb),
-         w_season = mean(w),
-         ha_season = mean(ha),
-         bba_season = mean(bba),
-         soa_season = mean(soa),
-         hra_season = mean(hra),
-         att_season = mean(attendance)) %>% 
-  ungroup() %>% 
-  mutate(r_per_season = (r-r_season)/g,
-         ab_per_season = (ab-ab_season)/g,
-         h_per_season = (h-h_season)/g,
-         hr_per_season = (hr-hr_season)/g,
-         bb_per_season = (bb-bb_season)/g,
-         so_per_season = (so-so_season)/g,
-         sb_per_season = (sb-sb_season)/g,
-         hra_per_season = (hra-hra_season)/g,
-         w_per_season = (w-w_season)/g,
-         ha_per_season = (ha-ha_season)/g,
-         bba_per_season = (bba-bba_season)/g,
-         soa_per_season = (soa-soa_season)/g) 
+  mutate(r_season = (r- mean(r))/sd(r),
+         #ab_season = (ab - mean(ab))/sd(ab),
+         h_season = (h - mean(h))/sd(h),
+         hr_season = (hr - mean(hr))/sd(hr),
+         bb_season = (bb - mean(bb))/sd(bb),
+         so_season = (so - mean(so))/sd(so),
+         #sb_season = (sb - mean(sb))/sd(sb),
+         #w_season = (w - mean(w))/sd(w),
+         ra_season = -(ra - mean(ra))/sd(ra),
+         ha_season = -(ha - mean(ha))/sd(ha),
+         bba_season = -(bba - mean(bba))/sd(bba),
+         soa_season = -(soa - mean(soa))/sd(soa),
+         hra_season = -(hra - mean(hra))/sd(hra))
 
+
+  
 std_teams %>% 
-  pivot_longer(cols = ends_with("per_season"), names_to = "stat_type", values_to = "stat") %>% 
+  filter(franch_id %in% current_teams) %>% 
+  pivot_longer(cols = ends_with("_season"), names_to = "stat_type", values_to = "stat") %>% 
   ggplot(aes(year_id, stat, color = stat_type)) +
   geom_line() +
   facet_wrap(~franch_id, scales = "free_x")
@@ -109,10 +69,10 @@ cpt_fct <- function(dat){
 }
 
 cpt <- lapply(1:30, function(i){
-  select(curt_teams_data[[i]], year_id, ends_with("per_season")) %>% 
+  select(curt_teams_data[[i]], year_id, ends_with("_season")) %>% 
     pivot_longer(-year_id, names_to = "stat") %>% 
     pivot_wider(names_from = "year_id", values_from = "value") %>% 
-    mutate(stat = str_remove_all(stat, "_per_season")) %>% 
+    mutate(stat = str_remove_all(stat, "_season")) %>% 
     select(-1) %>% 
     cpt_fct()
 })
@@ -129,10 +89,10 @@ cpt_var_fct <- function(dat){
                 years = cpt_years))
 }
 cpt_var <- lapply(1:30, function(i){
-  select(curt_teams_data[[i]], year_id, ends_with("per_season")) %>% 
+  select(curt_teams_data[[i]], year_id, ends_with("_season")) %>% 
     pivot_longer(-year_id, names_to = "stat") %>% 
     pivot_wider(names_from = "year_id", values_from = "value") %>% 
-    mutate(stat = str_remove_all(stat, "_per_season")) %>% 
+    mutate(stat = str_remove_all(stat, "_season")) %>% 
     select(-1) %>% 
     cpt_var_fct()
 })
@@ -143,7 +103,8 @@ total_values <- bind_rows(mean_change, var_change, .id = "type") %>%
   mutate(type = ifelse(type == 1, "mean", "variance"))
 
 std_teams %>% 
-  pivot_longer(cols = ends_with("per_season"), names_to = "stat_type", values_to = "stat") %>% 
+  filter(franch_id %in% current_teams) %>% 
+  pivot_longer(cols = ends_with("_season"), names_to = "stat_type", values_to = "stat") %>%  
   ggplot(aes(year_id, stat, color = stat_type)) +
   geom_line() +
   geom_vline(data = total_values, aes(xintercept = years, linetype = type)) +
